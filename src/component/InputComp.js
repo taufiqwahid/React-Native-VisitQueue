16c38ad8067fc408f7f580aa4551c724d42dc6f6
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-10-25 14:28:08 +0800

    update component

--- src/component/InputComp.js
+++ src/component/InputComp.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 import {stylesColors} from '../utils/stylesColors';
 import {stylesTexts} from '../utils/stylesTexts';
 
-const InputComp = ({title, password}) => {
+const InputComp = ({title, password, onChangeText}) => {
   return (
     <View>
       <Text style={{...stylesTexts.largeNormal, color: stylesColors.black2}}>
         {title}
       </Text>
       <TextInput
-        maxLength={10}
+        keyboardType={title.toLowerCase()}
+        maxLength={30}
         secureTextEntry={password}
+        onChangeText={text => onChangeText(text)}
         style={{
-          ...stylesTexts.mediumBold,
+          ...stylesTexts.defaultBold,
           padding: 10,
           color: stylesColors.default2,
           borderRadius: 10,
