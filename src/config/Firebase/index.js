ec29c4fe0d73ac6984e5673d715b4d5016c35fe2
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-10-25 14:28:49 +0800

    add firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNLDRrSqo01NqJMrr7tO2h2fy-VswcLHA',
+  authDomain: 'visitqueue-c0d40.firebaseapp.com',
+  databaseURL: 'https://visitqueue-c0d40-default-rtdb.firebaseio.com',
+  projectId: 'visitqueue-c0d40',
+  storageBucket: 'visitqueue-c0d40.appspot.com',
+  messagingSenderId: '452838845853',
+  appId: '1:452838845853:web:18b3df63d3656626a05374',
+  measurementId: 'G-PHXH8L4797',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default Firebase = app;
